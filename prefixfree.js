407bcbafd46568109c811af2c68af1f7bc85808f
Lea Verou <me@leaverou.me>
2011-10-29 13:38:57 +0300

    Made property replacement more robust

--- prefixfree.js
+++ prefixfree.js
@@ -156,7 +156,7 @@ var self = window.PrefixFree = {
 		
 		fix('functions', '(\\s|:)', '\\s*\\(', '$1' + prefix + '$2(');
 		fix('keywords', '(\\s|:)', '(\\s|;|\\})', '$1' + prefix + '$2$3');
-		fix('properties', '\\b', '\\s*:', prefix + '$1:');
+		fix('properties', '(\\{|\\s|;)', '\\s*:', '$1' + prefix + '$2:');
 		
 		// Prefix properties *inside* values (issue #8)
 		if (self.properties.length) {
