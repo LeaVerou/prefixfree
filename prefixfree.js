bcc862e188467efbf7c226fbf85c282299f25ead
naifamoodi <prince_of_nights@hotmail.com>
2012-12-14 22:54:38 +0530

    Update prefixfree.js

--- prefixfree.js
+++ prefixfree.js
@@ -76,7 +76,11 @@ var self = window.StyleFix = {
 					style.media = link.media;
 					style.disabled = link.disabled;
 					style.setAttribute('data-href', link.getAttribute('href'));
-					
+
+					if(link.hasAttribute('id')) {
+						style.setAttribute('id', link.getAttribute('id'));
+					}
+
 					parent.insertBefore(style, link);
 					parent.removeChild(link);
 					
